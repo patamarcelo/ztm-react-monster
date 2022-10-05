4b682647b5b5eda8594ea943462c2fbd1a35d21e
marcelo pata <patamarcelo@gmail.com>
2022-10-05 09:18:55 -0300

    adjusts

--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 // import ReactDOM from 'react-dom/client';
-// import { createRoot } from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
+const root = createRoot(container);
 root.render(
 	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
+		<App />
 	</React.StrictMode>
 );
 
